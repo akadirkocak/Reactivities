e625fb2c9ba7bcd95bf239c71178ba5055d36e63
akadirkocak <kadir.kocak.50@hotmail.com>
2023-01-05 14:39:22 +0100

    EditMode

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -9,6 +9,8 @@ function App() {
 
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined)
+  const [editMode, setEditMode] = useState(false)
+
 
   useEffect(() => {
     axios.get<Activity[]>('http://localhost:5051/api/activities')
@@ -23,16 +25,26 @@ function App() {
   function handleCancelSelectActivity() {
     setSelectedActivity(undefined)
   }
+  function handleFormOpen(id?: string) {
+    id ? handleSelectActivity(id) : handleCancelSelectActivity()
+    setEditMode(true);
+  }
+  function handleFormClose() {
+    setEditMode(false);
+  }
 
   return (
     <>
-      <NavBar />
+      <NavBar openForm={handleFormOpen} />
       <Container style={{ marginTop: '7em' }}>
         <ActivityDashboard
           activities={activities}
           selectedActivity={selectedActivity}
           selectActivity={handleSelectActivity}
           cancelSelectActivity={handleCancelSelectActivity}
+          editMode={editMode}
+          openForm={handleFormOpen}
+          closeForm={handleFormClose}
         />
       </Container>
     </>
