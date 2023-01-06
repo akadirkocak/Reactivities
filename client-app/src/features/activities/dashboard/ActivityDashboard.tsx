168fa228b37540c3584b7c4498d41c8166407043
akadirkocak <kadir.kocak.50@hotmail.com>
2023-01-07 00:50:58 +0100

    Deleting an activity using MobX

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -8,12 +8,11 @@ import ActivityList from './ActivityList';
 
 interface Props {
     activities: Activity[];
-    createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
     submitting: boolean
 }
 
-export default observer(function ActivityDashboard({ activities, createOrEdit, deleteActivity, submitting}: Props) {
+export default observer(function ActivityDashboard({ activities, deleteActivity, submitting}: Props) {
     const {activityStore} = useStore()
     const {selectedActivity, editMode} = activityStore
     return (
@@ -29,9 +28,7 @@ export default observer(function ActivityDashboard({ activities, createOrEdit, d
                 {selectedActivity && !editMode &&
                     <ActivityDetails />}
                 {editMode &&
-                    <ActivityForm                        
-                        createOrEdit={createOrEdit} 
-                        submitting={submitting}/>}
+                    <ActivityForm />}
             </Grid.Column>
 
         </Grid>
