605445e7d3d571eaece60d865184db7c922baed8
akadirkocak <kadir.kocak.50@hotmail.com>
2023-01-07 00:22:44 +0100

    Setting up MobX, Selecting an Activity using MobX

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -7,52 +7,33 @@ import { v4 as uuid } from 'uuid';
 import React from 'react';
 import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
+import { useStore } from '../../stores/store';
+import { observable } from 'mobx';
+import { observer } from 'mobx-react-lite';
 
 
 function App() {
   //States
+
+  const { activityStore } = useStore() 
+
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
-  const [loading, setLoading] = useState(true)
   const [submitting, setSubmitting] = useState(false)
 
+
   useEffect(() => {
-    agent.Activities.list().then(response => {
-      let activities: Activity[] = []
-      response.forEach(activity => {
-        activity.date = activity.date.split('T')[0]
-        activities.push(activity)
-      })
-      setActivities(activities);
-      setLoading(false)
-    })
-  }, [])
-
-  function handleSelectActivity(id: string) {
-    setSelectedActivity(activities.find(x => x.id === id));
-  }
-
-  function handleCancelSelect() {
-    setSelectedActivity(undefined);
-  }
-
-  function handleFormOpen(id?: string) {
-    id ? handleSelectActivity(id) : handleCancelSelect();
-    setEditMode(true);
-  }
-
-  function handleFormClose() {
-    setEditMode(false);
-  }
+    activityStore.loadActivities();
+  }, [activityStore])
 
   function handleDeleteActivity(id: string) {
     setSubmitting(true)
-    agent.Activities.delete(id).then(()=>{
+    agent.Activities.delete(id).then(() => {
       setActivities([...activities.filter(x => x.id !== id)])
       setSubmitting(false)
     })
-    
+
   }
 
   function handleCreateOrEditActivity(activity: Activity) {
@@ -74,19 +55,13 @@ function App() {
       })
     }
   }
-  if (loading) return <LoadingComponent content={'Loading app'} />
+  if (activityStore.loadingInitial) return <LoadingComponent content={'Loading app'} />
   return (
     <>
-      <NavBar openForm={handleFormOpen} />
+      <NavBar />
       <Container style={{ marginTop: '7em' }}>
         <ActivityDashboard
-          activities={activities}
-          selectedActivity={selectedActivity}
-          selectActivity={handleSelectActivity}
-          cancelSelectActivity={handleCancelSelect}
-          editMode={editMode}
-          openForm={handleFormOpen}
-          closeForm={handleFormClose}
+          activities={activityStore.activities}
           createOrEdit={handleCreateOrEditActivity}
           deleteActivity={handleDeleteActivity}
           submitting={submitting}
@@ -96,4 +71,4 @@ function App() {
   );
 }
 
-export default App;
+export default observer(App);
